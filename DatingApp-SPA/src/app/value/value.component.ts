43735a724d4e8b7b71efcb2971250b706e39e082
akzin05 <aungkhantzin50@gmail.com>
2020-08-26 22:49:08 +0800

    Add Login and JWT

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -10,12 +10,10 @@ export class ValueComponent implements OnInit {
   values: any;
   constructor(private http: HttpClient) { }
 
-  // tslint:disable-next-line: typedef
   ngOnInit() {
     this.getValues();
   }
 
-  // tslint:disable-next-line: typedef
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
